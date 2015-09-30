47b20b3d3c461ee6f436fe931c71965b2364046d
mussrya <ryan.musselwhite@gmail.com>
2015-09-30 10:57:07 +0100

    Working on new bestTimes function

--- nodeApp/nodeTest.js
+++ nodeApp/nodeTest.js
@@ -80,7 +80,7 @@ Stations.aggregate({
 
 var currentTime = new Date();
 var endTime = new Date();
-var startTime = new Date(endTime.getFullYear(), endTime.getMonth(), endTime.getDate(), 10, 00);
+var startTime = new Date(endTime.getFullYear(), endTime.getMonth(), endTime.getDate(), 09, 00);
 var endTime = new Date(endTime.getFullYear(), endTime.getMonth(), endTime.getDate(), 18, 50);
 var day = endTime.getDay();
 
