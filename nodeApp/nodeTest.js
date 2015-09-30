1866b4e2fc05250c79a4c78025d1398b30f63cbc
mussrya <ryan.musselwhite@gmail.com>
2015-09-30 20:26:41 +0100

    Working on new bestTimes function

--- nodeApp/nodeTest.js
+++ nodeApp/nodeTest.js
@@ -66,7 +66,7 @@ mongoose.connect('mongodb://localhost/cycleHire', function () {
 
     var results = [];
     
-    cursor.each(function (error, station) {
+    cursor.on(function (error, station) {
         results.push(station);
         //console.log(station);
         // Need a call back here to allow the console.log(results) further down to have results
